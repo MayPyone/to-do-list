1019b3c9a837a390f3b3efc3f0ab5efab83be783
May Pyone <maypyone015@gmail.com>
2023-07-19 22:51:12 +0630

    Fix bugs

--- src/input.js
+++ src/input.js
@@ -4,35 +4,33 @@ export const create = () => {
   }
   return JSON.parse(localStorage.getItem('todo'));
 };
- 
+
 const input = document.querySelector('.main');
 export const mydata = () => {
-
- input.addEventListener('keypress', (e) => {
-   if (e.key === 'Enter') {
+  input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
       const totaltasks = create();
-        const task = {
-            check: true,
-            des: document.querySelector('.main').value,
-            index:totaltasks.length,
-          };
-          totaltasks.push(task);
-    localStorage.setItem('todo', JSON.stringify(totaltasks));
-    console.log(totaltasks.length);
+      const task = {
+        check: true,
+        des: document.querySelector('.main').value,
+        index: totaltasks.length,
+      };
+      totaltasks.push(task);
+      localStorage.setItem('todo', JSON.stringify(totaltasks));
     }
   });
 };
 
 export const display = () => {
-    const inner = document.querySelector('.show');
-    const totaltasks = create();
-    for (let i = 0; i < totaltasks.length ;i +=1){
-        inner.innerHTML += `
+  const inner = document.querySelector('.show');
+  const totaltasks = create();
+  for (let i = 0; i < totaltasks.length; i += 1) {
+    inner.innerHTML += `
        <div class="container"> 
        <input type="checkbox" id="vehicle2" name="tasks" value="data">
        <input class="data-input" value= "${totaltasks[i].des}">
        <i class="fa-solid fa-ellipsis-vertical"></i>
        </div>
-        ` 
-    }
-}
+        `;
+  }
+};

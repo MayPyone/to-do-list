b8e8c7db2ea38c1803d9f03035d07302896cc791
May Pyone <maypyone015@gmail.com>
2023-07-19 22:10:02 +0630

    Add show book

--- src/input.js
+++ src/input.js
@@ -1,6 +1,42 @@
-// export const create = () => {
-//   if (localStorage.getItem('todo') == null) {
-//     localStorage.setItem('todo', JSON.stringify([]));
-//   }
-//   return JSON.parse(localStorage.getItem('todo'));
-// };
+import Icon from './icon.png';
+
+export const create = () => {
+  if (localStorage.getItem('todo') == null) {
+    localStorage.setItem('todo', JSON.stringify([]));
+  }
+  return JSON.parse(localStorage.getItem('todo'));
+};
+ 
+const input = document.querySelector('.main');
+
+export const mydata = () => {
+
+ input.addEventListener('keypress', (e) => {
+    
+    if (e.key === 'Enter') {
+        const totaltasks = create();
+        const task = {
+            check: true,
+            des: document.querySelector('.main').value,
+            index:totaltasks.length,
+          };
+          totaltasks.push(task);
+    localStorage.setItem('todo', JSON.stringify(totaltasks));
+    console.log(totaltasks.length);
+    }
+  });
+};
+
+export const display = () => {
+    const inner = document.querySelector('.show');
+    const totaltasks = create();
+    for (let i = 0; i < totaltasks.length ;i +=1){
+        inner.innerHTML += `
+       <div class="container"> 
+       <input type="checkbox" id="vehicle2" name="tasks" value="data">
+       <input class="data-input" value= "${totaltasks[i].des}">
+       <i class="fa-solid fa-ellipsis-vertical"></i>
+       </div>
+        ` 
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,10 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
 
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
 
     ],
 
